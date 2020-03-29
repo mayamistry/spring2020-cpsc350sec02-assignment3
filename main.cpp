2185f30406a2291773c02843bb222b7d1149353f
Maya Mistry <mistry@chapman.edu>
2020-03-29 22:44:58 +0000

    Made some small adjustments

--- main.cpp
+++ main.cpp
@@ -6,26 +6,29 @@ int main(int argc, char**argv) {
     string filePath = "";
     if (argc > 1) {
       filePath = argv[1];
-    }
 
-    //boolean variable to check if the program should exit or not (if source code has an error, it will exit)
-    bool error = false;
+      //boolean variable to check if the program should exit or not (if source code has an error, it will exit)
+      bool error = false;
 
-    while (error != true) {
-      //instantiating the FileIO object to be of a type char (not  a template anymore)
-      FileIO<char> *file = new FileIO<char>(filePath);
-      error = file->readInFile();
-      delete file;
-      if (error == true) {
-        break;
-      } else {
-        //if the program didn't have errors, the user can check another file
-        cout << "Your file was completely correct! Enter a file path for another source code you want to check. If you want to quit, enter 'exit'." << endl;
-        cin >> filePath;
-        if (filePath == "exit") {
+      while (error != true) {
+        //instantiating the FileIO object to be of a type char (not  a template anymore)
+        FileIO<char> *file = new FileIO<char>(filePath);
+        error = file->readInFile();
+        delete file;
+        if (error == true) {
           break;
+        } else {
+          //if the program didn't have errors, the user can check another file
+          cout << "Your file was completely correct! Enter a file path for another source code you want to check. If you want to quit, enter 'exit'." << endl;
+          cin >> filePath;
+          if (filePath == "exit") {
+            break;
+          }
         }
       }
+    } else {
+      cout << "USAGE: ./a.out [input string]" << endl;
+      return 1;
     }
   return 0;
 }
